019ed978f65ca982faf3692610aa12605bf8c9df
Som Snytt <som.snytt@gmail.com>
2023-06-14 00:55:45 -0700

    more

--- tests/pos/i17969.scala
+++ tests/pos/i17969.scala
@@ -1,13 +1,18 @@
 
-// scalac: -Werror
+// scalac: -deprecation -Werror
 
 import scala.annotation.nowarn
 object Foo {
   private def bar[T](body: => T): T = body
+
+  @deprecated("unwise", since="0.5")
+  def f = 42
 
   def foo(): String = bar {
     @nowarn("cat=deprecation")
     val m = implicitly[Manifest[String]]
     m.runtimeClass.getName
   }
+
+  def g = f
 }
