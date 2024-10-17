e3f0b1f7074f762fc91d73e01f2b4c806a82f27a
Som Snytt <som.snytt@gmail.com>
2024-10-17 10:54:29 -0700

    Root of Java select must be class or rooted package

--- tests/pos/t11788/Foo.java
+++ tests/pos/t11788/Foo.java
@@ -1,6 +1,7 @@
 public class Foo {
 	private String java;
 
+	// java is the rooted package, not the field
 	public java.lang.Integer test() {
 		//return Integer.valueOf(42);
 		throw null;
---
+++ tests/pos/t11788c/Foo.java
@@ -0,0 +1,8 @@
+public class Foo {
+	private String java;
+
+	// java is class in scope, not the term member or package
+	public java.lang.Integer.Inner test() {
+		throw null;
+	}
+}
