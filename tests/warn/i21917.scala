b7c8c875f465c1a692ad903192d0c9b77ad1db93
Som Snytt <som.snytt@gmail.com>
2024-12-27 17:47:49 -0800

    tweak conditions for warning
    Handle match types
    No transparent inline exclusion

--- tests/warn/i21917.scala
+++ tests/warn/i21917.scala
@@ -1,21 +1,31 @@
-//> using options -Wunused:all
+//> using scala 3.6.2
+//> using options -Wunused:imports
+//
+//> abusing scala 2.13.15
+//> abusing options -Wunused:imports -Xsource:3
 
 import Pet.Owner
 
 class Dog(owner: Owner) extends Pet(owner) {
-  import Pet.*
+  import Pet.* // warn although unambiguous
+  //import Car.* // ambiguous
 
   def bark(): String = "bite"
 
-  def this(owner: Owner, goodDog: Boolean) =
+  def this(owner: Owner, goodDog: Boolean) = {
     this(owner)
-    if goodDog then println(s"$owner's dog is a good boy")
+    if (goodDog) println(s"$owner's dog is a good boy")
+  }
   
-  //val getOwner: Owner = owner
+  val getOwner: Owner = owner
 }
 
-trait Pet(owner: Owner)
+class Pet(val owner: Owner)
 
 object Pet {
+  class Owner
+}
+
+object Car {
   class Owner
 }
